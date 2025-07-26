5b54ca45d165c29bb56827e363b2034f473426a2
Lauris Neimanis <neimanis.lauris@gmail.com>
2025-07-26 17:57:28 +0300

    feat(app): add basic PHP app skeleton

---
+++ src/app/Hello.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class Hello
+{
+    public static function world(): string
+    {
+        return 'Hello, Docker CI Demo!';
+    }
+}
---
+++ src/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use App\Hello;
+
+echo '<h1>' . Hello::world() . '</h1>';
